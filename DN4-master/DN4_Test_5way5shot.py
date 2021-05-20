9ebe8265fb9862f2df2509a432703006d620a00d
Blair129 <44133060+Blair129@users.noreply.github.com>
2021-05-20 22:29:27 +0100

    add print

--- DN4-master/DN4_Test_5way5shot.py
+++ DN4-master/DN4_Test_5way5shot.py
@@ -183,6 +183,8 @@ def accuracy(output, target, topk=(1,)):
         for k in topk:
             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
+        print(len((res)))
+        print(res)
         return res
 
 
