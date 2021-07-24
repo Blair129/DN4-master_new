9bbae479b586ecb909df31d99d07fd2ae0bce780
Blair129 <44133060+Blair129@users.noreply.github.com>
2021-07-25 00:21:06 +0100

    add transformer

--- DN4-master/models/network_transformer.py
+++ DN4-master/models/network_transformer.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 from torch.nn import init
+import torch.nn.functional as F
 import functools
 import pdb
 import math
 import sys
+import numpy as np
 
 sys.dont_write_bytecode = True
 
@@ -148,6 +150,75 @@ def print_network(net):
     print('Total number of parameters: %d' % num_params)
 
 
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, q, k, v):
+        attn = torch.bmm(q, k.transpose(1, 2))
+        attn = attn / self.temperature
+        log_attn = F.log_softmax(attn, 2)
+        attn = self.softmax(attn)
+        attn = self.dropout(attn)
+        output = torch.bmm(attn, v)
+        return output, attn, log_attn
+
+
+class MultiHeadAttention(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
+        nn.init.normal_(self.w_qs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.w_ks.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_k)))
+        nn.init.normal_(self.w_vs.weight, mean=0, std=np.sqrt(2.0 / (d_model + d_v)))
+
+        self.attention = ScaledDotProductAttention(temperature=np.power(d_k, 0.5))
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.fc = nn.Linear(n_head * d_v, d_model)
+        nn.init.xavier_normal_(self.fc.weight)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v):
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, _ = q.size()
+        sz_b, len_k, _ = k.size()
+        sz_b, len_v, _ = v.size()
+
+        residual = q
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k)  # (n*b) x lq x dk
+        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k)  # (n*b) x lk x dk
+        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v)  # (n*b) x lv x dv
+
+        output, attn, log_attn = self.attention(q, k, v)
+
+        output = output.view(n_head, sz_b, len_q, d_v)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1)  # b x lq x (n*dv)
+
+        output = self.dropout(self.fc(output))
+        output = self.layer_norm(output + residual)
+
+        return output
+
+
 ##############################################################################
 # Classes: FourLayer_64F
 ##############################################################################
@@ -183,33 +254,37 @@ class FourLayer_64F(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=use_bias),
             norm_layer(64),
             nn.LeakyReLU(0.2, True),  # 64*21*21
+            nn.MaxPool2d(kernel_size=3, stride=3), # 64*7*7
 
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=use_bias),
             norm_layer(64),
-            nn.LeakyReLU(0.2, True),  # 64*21*21
+            nn.LeakyReLU(0.2, True),  # 64*7*7
         )
 
+        self.slf_attn = MultiHeadAttention(1, 64, 64, 64, dropout=0.5)
         self.imgtoclass = ImgtoClass_Metric(neighbor_k=neighbor_k)  # 1*num_classes
 
     def forward(self, input1, input2):
-
+        print
         # extract features of input1--query image
-        q = self.features(input1)
+        q = self.features(input1).view(15, 64, -1).permute(0, 2, 1)
+        q_trans = self.slf_attn(q, q, q)
         # print(q.shape)
 
         # extract features of input2--support set
         S = []
         for i in range(len(input2)):
-            support_set_sam = self.features(input2[i])
-            # # print(support_set_sam.shape)
+            support_set_sam = self.features(input2[i]).view(5, 64, -1).permute(0, 2, 1)
+            # print(support_set_sam.shape)
+            support_set_sam_trans = self.slf_attn(support_set_sam, support_set_sam, support_set_sam)
             # B, C, h, w = support_set_sam.size()
             # support_set_sam = support_set_sam.permute(1, 0, 2, 3)
             # support_set_sam = support_set_sam.contiguous().view(C, -1)
 
-            S.append(support_set_sam)
+            S.append(support_set_sam_trans)
 
         # print("-----------------"+str(len(S)))
-        x = self.imgtoclass(q, S)  # get Batch*num_classes
+        x = self.imgtoclass(q_trans, S)  # get Batch*num_classes
 
         return x
 
@@ -222,26 +297,104 @@ class ImgtoClass_Metric(nn.Module):
         super(ImgtoClass_Metric, self).__init__()
         self.neighbor_k = neighbor_k
 
-    def cal_SSIM(self, input1, input2):
+    # Calculate the k-Nearest Neighbor of each local descriptor
+    def cal_cosinesimilarity(self, input1, input2):
         B, C, h, w = input1.size()  # 15，64，21, 21
         Similarity_list = []
 
         for i in range(B):
             query_sam = input1[i]  # 64*21*21
-            query_sam_norm = torch.norm(query_sam, 2, 0, True)
-            query_sam = (query_sam / query_sam_norm).unsqueeze(0)
-            query_sam = query_sam.repeat(5, 1, 1, 1)
-
+            query_sam = query_sam.view(C, -1)  # 64*441
+            query_sam = torch.transpose(query_sam, 0, 1)  # 441*64
+            query_sam_norm = torch.norm(query_sam, 2, 1,
+                                        True)  # calculate 2-norm for each row (ie. for each descriptor)
+            query_sam = query_sam / query_sam_norm  # normalization
 
             if torch.cuda.is_available():
                 inner_sim = torch.zeros(1, len(input2)).cuda()  # 1*3
 
             for j in range(len(input2)):  # j = 0,1,2
-                support_set_sam = input2[j]  # 5*64*21*21
-                support_set_sam_norm = torch.norm(support_set_sam, 2, 1, True)
+                support_set_sam = input2[j]  # 64*2205(5*441)
+                support_set_sam_norm = torch.norm(support_set_sam, 2, 0,
+                                                  True)  # calculate 2-norm for each column(ie. for each descriptor)
                 support_set_sam = support_set_sam / support_set_sam_norm  # normalization
 
                 # cosine similarity between a query sample and a support category
+                innerproduct_matrix = query_sam @ support_set_sam  # 441*2205
+
+                # choose the top-k nearest neighbors
+                topk_value, topk_index = torch.topk(innerproduct_matrix, self.neighbor_k, 1)  # 441*3
+                inner_sim[0, j] = torch.sum(topk_value)  # sum mk value
+
+            Similarity_list.append(inner_sim)
+
+        Similarity_list = torch.cat(Similarity_list, 0)  # 15*3 (3 classes)
+
+        return Similarity_list
+
+    def cal_euclideandistance(self, input1, input2):
+        B, C, h, w = input1.size()
+        Similarity_list = []
+
+        for i in range(B):
+            query_sam = input1[i]
+            query_sam = query_sam.view(C, -1)
+            query_sam = torch.transpose(query_sam, 0, 1)
+            # print(query_sam.shape)
+            # query_sam_norm = torch.norm(query_sam, 2, 1, True)
+            # query_sam = query_sam/query_sam_norm
+
+            if torch.cuda.is_available():
+                inner_sim = torch.zeros(1, len(input2)).cuda()
+                # print("inner_sim"+str(inner_sim.shape))
+
+            for j in range(len(input2)):
+                support_set_sam = input2[j]
+                # print(support_set_sam.shape)
+                support_set_sam = torch.transpose(support_set_sam, 0, 1)
+                # support_set_sam_norm = torch.norm(support_set_sam, 2, 0, True)
+                # support_set_sam = support_set_sam/support_set_sam_norm
+
+                # euclidean distance between a query sample and a support category
+                innerproduct_matrix = torch.cdist(query_sam, support_set_sam, p=2)
+                # print("innerproduct"+str(innerproduct_matrix.shape))
+
+                # choose the top-k nearest neighbors
+                topk_value, topk_index = torch.topk(innerproduct_matrix, self.neighbor_k, 1, largest=False)
+                # print(topk_value, topk_index)
+                inner_sim[0, j] = torch.sum(topk_value)
+
+            Similarity_list.append(inner_sim)
+
+        Similarity_list = torch.cat(Similarity_list, 0)
+        # print(Similarity_list.shape)
+
+        return Similarity_list
+
+    def cal_SSIM(self, input1, input2):
+        # print(input1.shape) # 15*49*64
+
+        input1 = input1.view(15, 7, 7, 64).permute(0, 3, 1, 2)
+
+        B, C, h, w = input1.size()  # 15，64，7, 7
+        Similarity_list = []
+
+        for i in range(B):
+            query_sam = input1[i]  # 64*7*7
+            query_sam_norm = torch.norm(query_sam, 2, 0, True)
+            query_sam = (query_sam / query_sam_norm).unsqueeze(0)
+            query_sam = query_sam.repeat(5, 1, 1, 1)
+
+            if torch.cuda.is_available():
+                inner_sim = torch.zeros(1, len(input2)).cuda()  # 1*3
+
+            for j in range(len(input2)):  # j = 0,1,2
+                support_set_sam = input2[j].permute(0, 2, 1).view(5, 64, 7, 7)  # 5*64*7*7
+                # print(support_set_sam.shape)
+                support_set_sam_norm = torch.norm(support_set_sam, 2, 1, True)
+                support_set_sam = support_set_sam / support_set_sam_norm  # normalization
+
+                # SSIM between a query sample and a support category
                 innerproduct_matrix = ssim(query_sam, support_set_sam, data_range=1, size_average=False)
 
                 # choose the top-k nearest neighbors
@@ -353,3 +506,5 @@ class ResNetLike(nn.Module):
         x = self.imgtoclass(q, S)  # get Batch*num_classes
 
         return x
+
+
